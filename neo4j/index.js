6785b6a50f1bfc338a571db598e3d41b089c9650
ryancmoniz <ryancmoniz@gmail.com>
2020-06-03 14:20:28 -0400

    Adjust environment variables for deployment

--- neo4j/index.js
+++ neo4j/index.js
@@ -1,6 +1,8 @@
 const neo4j = require('neo4j-driver');
 const keys = require('../keys.js');
 
-const driver = neo4j.driver('bolt://localhost:7687/', neo4j.auth.basic(keys.user, keys.pass), { disableLosslessIntegers: true });
+const user = process.env.NEO4J_USER || keys.user;
+const pass = process.env.NEO4J_PASS || keys.pass;
+const driver = neo4j.driver('bolt://18.220.31.247:7687/', neo4j.auth.basic(user, pass), { disableLosslessIntegers: true });
 
 module.exports = driver;
